c8fdeb327e05a3889f558856a34cddbacd924785
user <zzang2314274@hanmail.net>
2017-11-20 17:17:07 +0900

    4th time! 4th!

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,8 @@ def post_new(request):
 
 def post_edit(request, pk):
      post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(instance=post)
+    if request.method == "GET":
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
