9e7d406e512a6d21cac5dc1adcb9d1c37b878ed2
user <zzang2314274@hanmail.net>
2017-11-20 17:02:30 +0900

    Checking edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     # 도메인 다음에 아무것도 없을 경우(정규 표현식),
     # views 파일의 post_list 클래스로 이동시킨다.
 ]
--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,21 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+            return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request, pk):
+     post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form':form})
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
 # 위의 시간대에 따라 받아온 게시글의 행렬을, 아래의 형태로 post_list.html에 뿌린다.
