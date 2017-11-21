e18d57cc99611172ee8a2edfbc71e73df08568f9
user <zzang2314274@hanmail.net>
2017-11-21 18:00:36 +0900

    Fixing Draft

--- blog/views.py
+++ blog/views.py
@@ -42,7 +42,7 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull==True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
