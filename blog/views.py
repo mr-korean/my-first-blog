d1d746df28161a04292935889cbc841687617018
user <zzang2314274@hanmail.net>
2017-11-21 18:15:58 +0900

    Fixing minor problems

--- blog/views.py
+++ blog/views.py
@@ -22,9 +22,9 @@ def post_new(request):
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
-            return render(request, 'blog/post_edit.html', {'form':form})
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form':form})
 
 @login_required
 def post_edit(request, pk):
